d624a395eca96e0ef17ba96081e1111c1a987b3d
kyle8285 <kylelussier3@gmail.com>
2015-03-23 23:03:17 -0400

    updated, added requests, moment

--- git_text/views.py
+++ git_text/views.py
@@ -8,11 +8,22 @@ import models
 from git_text import app
 from database import session
 
+import requests
+
+import moment
+from datetime import datetime
+
 
 
 @app.route("/", methods=["GET"])
 def get_updates():
-	return "Did you commit to your github today?"
+	git_commits = get_user_commits()
+	print type(git_commits)
+	if git_commits["total_count"] > 0:
+		return "yayyyyy"
+
+	return "nayyyy"
+
 
 @app.route("/commits", methods=["GET"])
 def get_commits():
@@ -23,4 +34,18 @@ def get_commits():
 
 	data = json.dumps([commit.as_dictionary() for commit in commits])
 	return Response(data, 200, mimetype="application/json")
+
+def get_user_commits():
+	today = moment.now().format("YYYY-M-D")
+	#print moment.now()
+	url = 'https://api.github.com/search/repositories?q=user:kyle8285+pushed:{}'.format(today)
+	try:
+		r = requests.get(url)
+		return r.json()
+	except: # this needs to be fixed
+		return "fix this"
+
+
+
+
 
