66f8acaf45d3bea076f05785ec48ab224aedfbcb
kyle8285 <kylelussier3@gmail.com>
2015-04-02 11:22:26 -0400

    updated

--- test.py
+++ test.py
@@ -1,7 +1,21 @@
-print "hello world"
-
-# @sched.scheduled_job('cron', hour=22):
-# def ping():
-# send get request
-
 # http://apscheduler.readthedocs.org/en/3.0/
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+import requests
+sched = BlockingScheduler()
+
+
+@sched.scheduled_job('interval', minutes=20)
+def job():
+    # send get request to "/" route
+    r = requests.get('/')
+    print 'This job is run every minute.'
+
+@sched.scheduled_job('cron', hour=22):
+def ping():
+    #send get request to "/test"
+    r = requests.get('/test')
+    print "This pings the actual route"
+
+
+sched.start()
